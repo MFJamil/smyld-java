1612428fbbad723845087d55c7c9c45e8c190884
MFJamil <mohammedfjameel@yahoo.com>
2019-10-03 19:37:31 +0200

    Project (EP): adding documentation pictures

--- apps/ep/EntityPlot/src/test/java/org/smyld/app/ep/EntityPlotTester.java
+++ apps/ep/EntityPlot/src/test/java/org/smyld/app/ep/EntityPlotTester.java
@@ -15,16 +15,20 @@ public class EntityPlotTester {
     }
 
     public static void main(String[] args)throws Exception {
-        new EntityPlotTester().testFlowChartDiagram();
+        String filesPath = "/media/mfjamil/Ubuntu_second/work/java/smyld-java/apps/ep/EntityPlot/src/test/resources/files/";
+        new EntityPlotTester().testFlowChartDiagram(filesPath + "EPFlowChart.xml");
+        new EntityPlotTester().testFlowChartDiagram(filesPath + "EPFlowChart_NoShadow.xml");
+        new EntityPlotTester().testFlowChartDiagram(filesPath + "EPFlowChart_NoShadow_NoReflect.xml");
+        new EntityPlotTester().testFlowChartDiagram(filesPath + "EPFlowChart_NoShadow_NoReflect_NoImage.xml");
     }
     @Test
-    public void testFlowChartDiagram() throws Exception{
+    public void testFlowChartDiagram(String filename) throws Exception{
         JFrame frm = new JFrame("Billing Configuration");
         frm.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frm.setSize(1000, 800);
 
         // Initializing the plotter here
-        FlowChartPlotter pnlProductPlot = new FlowChartPlotter(new EPFlowChartXmlReader("/media/mfjamil/Ubuntu_second/work/java/smyld-java/apps/ep/EntityPlot/src/test/resources/files/EPFlowChart.xml"));
+        FlowChartPlotter pnlProductPlot = new FlowChartPlotter(new EPFlowChartXmlReader(filename));
         JScrollPane sp = new JScrollPane(pnlProductPlot);
         pnlProductPlot.setScrollContainer(sp);
         sp.getViewport().add(pnlProductPlot);
