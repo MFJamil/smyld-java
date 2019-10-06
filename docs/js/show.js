f4ae8a176ff623843efda11b770559fdfa5fdb3d
MFJamil <36378263+MFJamil@users.noreply.github.com>
2019-10-06 23:08:12 +0200

    Update show.js

--- docs/js/show.js
+++ docs/js/show.js
@@ -1,1 +1,3 @@
-document.output("Testttttt");
+document.write("<svg width=\"100\" height=\"100\">");
+document.write("<circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"green\" stroke-width=\"4\" fill=\"yellow\" />");
+document.write("</svg>");
